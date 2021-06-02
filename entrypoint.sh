2fd14e67d1523423e8c91dfee879e5afcfbf0312
Yannick M. Dixken <yannick@dixken.de>
2021-06-02 16:41:11 +0200

    loglevel, cors

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export KIEBITZ_SETTINGS=`readlink -f /srv/backend/settings/${environment}/`
 echo -e "Starting Service ${service} in Environment: ${environment}"
-`which kiebitz` run "${service}"
+`which kiebitz --level debug` run "${service}" 
